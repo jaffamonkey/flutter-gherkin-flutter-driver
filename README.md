406fdd7c68ebb34b9d07ff5aa6e4e9168b2e5a24
pablo <pablo@pablos-mbp.home>
2021-09-05 20:33:38 +0100

    updates

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # example_with_flutter_driver
 
 ```
-flutter drive --target=./test_driver/app.dart --flavor=dev
+flutter drive -v --target=./test_driver/app.dart
 ```
---
+++ run-tests-ios.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo 'BITBAR IOS APP TEST SHELL SCRIPT'
+echo 'Extracting tests.zip...'
+yes | unzip tests.zip
+
+mv application.ipa application.zip
+
+unzip application.zip
+
+mv Payload/Runner.app Runner.app
+
+# make sure build folder exists
+mkdir -p flutter-gherkin-integration-test/build/ios/iphoneos
+
+# move app to build folder
+mv Runner.app flutter-gherkin-integration-test/build/ios/iphoneos/Runner.app
+
+echo 'starting flutter doctor ...'
+flutter-2.2.0 doctor
+
+flutter-2.2.0 devices
+
+cd flutter-gherkin-integration-test
+
+# Install flutter packages
+flutter-2.2.0 pub get
+
+flutter-2.2.0 drive -v --target=./test_driver/app.dart
