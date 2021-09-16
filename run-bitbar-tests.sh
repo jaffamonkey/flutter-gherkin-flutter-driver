fba18a14693d9868acd1a1d69b405130d1113538
Paul Littlebury <paullittlebury@gmail.com>
2021-09-16 03:58:41 +0100

    Update run-bitbar-tests.sh

--- run-bitbar-tests.sh
+++ run-bitbar-tests.sh
@@ -6,7 +6,7 @@ filedownload_Path="https://cloud.bitbar.com/cloud/api/v2/me/files"
 
 # You can add other user-entered variables here
 echo "Enter your BitBar API Key, followed by [ENTER]:"
-read -r BB_APIKEY
+read -r bb_api_key
 echo "Enter name for test run, followed by [ENTER]:"
 read -r testrunname
 echo "Rename template test run file"
@@ -17,7 +17,7 @@ cp $script_Path/android/android-configuration-tmp configuration-android-"$testru
 echo "Compress app source folder for upload ..."       
 zip -r ../android-test-files-"$testrunname".zip run-tests.sh ../flutter-gherkin-flutter-driver
 echo "Upload files to BitBar cloud"
-response4=$(curl -X POST -u "$BB_APIKEY": $bitbar_Path/files -F "file=@android-test-files-$testrunname.zip")
+response4=$(curl -X POST -u "$bb_api_key": $bitbar_Path/files -F "file=@android-test-files-$testrunname.zip")
 response4=("${response4[*]}") # convert to array
 body=${response4[*]} # get all elements
 fileid=$(echo "$body" | jq '.id')
@@ -29,10 +29,10 @@ echo "Add test run name to configuration file (Build run number)"
 sed -i .bak "s/TEST_RUN_NAME/$testrunname/" configuration-android-"$testrunname"
 rm ./*.bak
 echo "Set build/test cycle running on BitBar, using configuration file ..."
-curl -H 'Content-Type:application/json' -u "$BB_APIKEY": $bitbar_Path/runs --data-binary @configuration-android-"$testrunname"
+curl -H 'Content-Type:application/json' -u "$bb_api_key": $bitbar_Path/runs --data-binary @configuration-android-"$testrunname"
 read -r -t 10 -p "10 second wait to allow for session delay ..."
 echo "Retrieve project test runs list and extract latest Test Run ID ..."
-response=$(curl -u "$BB_APIKEY": $bitbar_Path/projects/207073277/runs)
+response=$(curl -u "$bb_api_key": $bitbar_Path/projects/207073277/runs)
 response=("${response[*]}") # convert to array
 body=${response[*]} # get all elements
 runid=$(echo "$body" | jq '.data[0].id')
@@ -40,7 +40,7 @@ echo "Test Run ID: $runid"
 x=0
 while [ $x -le 1 ]; do
   read -r -t 20 -p "20 second wait before rechecking if test run has ended .."
-  response2=$(curl -u "$BB_APIKEY": $bitbar_Path/projects/207073277/runs/"$runid")
+  response2=$(curl -u "$bb_api_key": $bitbar_Path/projects/207073277/runs/"$runid")
   response2=("${response2[*]}") # convert to array
   body=${response2[*]} # get all elements
   runstatus=$(echo "$body" | jq '.state') 
@@ -55,13 +55,13 @@ while [ $x -le 1 ]; do
   fi
 done
 echo "Retreive device session ids for test run"
-response3=$(curl -u "$BB_APIKEY": $bitbar_Path/projects/207073277/runs/"$runid"/device-sessions)
+response3=$(curl -u "$bb_api_key": $bitbar_Path/projects/207073277/runs/"$runid"/device-sessions)
 response3=("${response3[*]}") # convert to array
 body=${response3[*]} # get all elements
 devicesessionid1=$(echo "$body" | jq '.data[0].id')
 echo "Device Session ID: $devicesessionid1"
 echo "Retrieve device 1 session reports"
-response7=$(curl -u "$BB_APIKEY": $bitbar_Path/projects/207073277/runs/"$runid"/device-sessions/"$devicesessionid1"/output-file-set/files)
+response7=$(curl -u "$bb_api_key": $bitbar_Path/projects/207073277/runs/"$runid"/device-sessions/"$devicesessionid1"/output-file-set/files)
 echo "$runid/$devicesessionid1"
 response7=("${response7[*]}") # convert to array
 body=${response7[*]} # get all elements
@@ -71,10 +71,10 @@ filetodownload2=$(echo "$body" | jq '.data[1].id')
 filetodownload3=$(echo "$body" | jq '.data[2].id')
 filetodownload4=$(echo "$body" | jq '.data[3].id')
 echo "$testrunname"
-curl -L -u "$BB_APIKEY": $filedownload_Path/"$filetodownload1"/file --output console-"$testrunname".log
-curl -L -u "$BB_APIKEY": $filedownload_Path/"$filetodownload2"/file --output performance-"$testrunname".json
-curl -L -u "$BB_APIKEY": $filedownload_Path/"$filetodownload3"/file --output output-files-"$testrunname".zip
-curl -L -u "$BB_APIKEY": $filedownload_Path/"$filetodownload4"/file --output device-"$testrunname".log
+curl -L -u "$bb_api_key": $filedownload_Path/"$filetodownload1"/file --output console-"$testrunname".log
+curl -L -u "$bb_api_key": $filedownload_Path/"$filetodownload2"/file --output performance-"$testrunname".json
+curl -L -u "$bb_api_key": $filedownload_Path/"$filetodownload3"/file --output output-files-"$testrunname".zip
+curl -L -u "$bb_api_key": $filedownload_Path/"$filetodownload4"/file --output device-"$testrunname".log
 ls -la
 if grep -Fq "FAILED (in 1 packages)" console-"$testrunname".log; then
   echo "Bitbar Test Run $testrunname has failed scenarios." 1>&2
