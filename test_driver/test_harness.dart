eebe9f11d5705e50e8bb74581355ef93ab441c05
Paul Littlebury <paullittlebury@gmail.com>
2021-09-30 22:49:33 +0100

    Update test_harness.dart

--- test_driver/test_harness.dart
+++ test_driver/test_harness.dart
@@ -10,10 +10,10 @@ Future<void> main() {
   )
     ..restartAppBetweenScenarios = true
     ..targetAppWorkingDirectory = '../'
-    ..targetAppPath = 'test_driver/app.dart';
+    ..targetAppPath = 'test_driver/app.dart'
+    ..tagExpression = '@smoke';
   // ..buildFlavor = "staging" // uncomment when using build flavor and check android/ios flavor setup see android file android\app\build.gradle
   // ..targetDeviceId = "all" // uncomment to run tests on all connected devices or set specific device target id
-  // ..tagExpression = '@smoke and not @ignore' // uncomment to see an example of running scenarios based on tag expressions
   // ..logFlutterProcessOutput = true // uncomment to see command invoked to start the flutter test app
   // ..verboseFlutterProcessLogs = true // uncomment to see the verbose output from the Flutter process
   // ..flutterBuildTimeout = Duration(minutes: 3) // uncomment to change the default period that flutter is expected to build and start the app within
